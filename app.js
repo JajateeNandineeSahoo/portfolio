56ef79cd751f63de04f1f14b173e754b532bd4a1
JajateeNandineeSahoo <jajateenandineesahoo@gmail.com>
2021-07-29 16:55:37 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+
+window.sr = ScrollReveal();
+sr.reveal('.navbar', {
+    duration: 2000,
+    delay: 1000,
+    origin:'left'
+});
+
+sr.reveal('.main-header', {
+    duration: 2000,
+    origin:'top',
+    distance:'300px'
+});
+
+sr.reveal('.main-header span', {
+    duration: 2000,
+    delay: 2000,
+    origin:'right',
+    distance:'300px',
+});
